c3b5c5221b5dd829d7d3f63b07545838407b70ed
freepina <freepina@gmail.com>
2016-05-24 23:19:09 -0400

    d

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, session, request
+from flask import redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET','POST'])
+def home():
+    return render_template("home.html")
+
+@app.route('/about', methods = ['GET','POST'])
+def about():
+    return render_template("about.html")
+
+if __name__ == '__main__':
+        app.secret_key = "hello"
+        app.debug = True
+        app.threaded = True
+        app.run(host='0.0.0.0', port=8000)
+else:
+        app.secret_key = "hello"
+        app.debug = True
