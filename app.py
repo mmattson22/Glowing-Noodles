73fbc6e31aca32c0f1459da165a59587c1f6b273
TiffL <lei.tiffany@gmail.com>
2016-06-06 19:56:16 -0400

    merge branches

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, session, request
+from flask import redirect, url_for
+import utils
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET','POST'])
+def home():
+    if request.method=="GET":
+        return render_template("home.html")
+    else:
+        newPost = request.form['newPost']
+        lostFound = request.form['LostOrFound']
+        tagsChosen = request.form['tagSuggest']
+        if tagsChosen == "Select a tag below:":
+            tagsChosen == None
+        print tagsChosen
+        userID = 1234
+        pic = "link"
+        if lostFound == "lost":
+            utils.writePost(userID, newPost, pic,"lost",tagsChosen)
+            return redirect(url_for("lost"))
+        elif lostFound == "found":
+            utils.writePost(userID, newPost, pic,"found",tagsChosen)
+            return redirect(url_for("found"))
+
+        return redirect(url_for("lost"))
+
+@app.route('/about', methods = ['GET','POST'])
+def about():
+    return render_template("about.html")
+
+@app.route('/lost', methods = ['GET','POST'])
+def lost():
+    if request.method=="GET":
+        lostPosts = utils.getAllPosts("lost")
+        return render_template("lost.html", lostPosts=lostPosts)
+
+@app.route('/found', methods = ['GET','POST'])
+def found():
+    if request.method=="GET":
+        foundPosts = utils.getAllPosts("found")
+        return render_template("found.html", foundPosts=foundPosts)
+    return render_template("found.html")
+
+if __name__ == '__main__':
+    app.secret_key = "hello"
+    app.debug = True
+    app.threaded = True
+    app.run(host='0.0.0.0', port=8000)
+else:
+    app.secret_key = "hello"
+    app.debug = True
---
+++ create.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+conn = sqlite3.connect("data.db")
+
+c = conn.cursor()
+
+q = "drop table if exists users"
+c.execute(q)
+q = "drop table if exists posts"
+c.execute(q)
+q = "drop table if exists foundPosts"
+c.execute(q)
+q = "drop table if exists comments"
+c.execute(q)
+
+q = "create table users(id integer, facebookid text)"
+c.execute(q)
+
+q = "create table posts(id integer, uid integer, content text, picture text, tagsChosen text)"
+c.execute(q)
+
+q = "create table foundPosts(id integer, uid integer, content text, picture text, tagsChosen text)"
+c.execute(q)
+
+q = "create table comments(id integer, pid integer, uid integer, content text)"
+c.execute(q)
+
+#q = "create table relations(uid integer, pid integer, cid integer)"
+#c.execute(q)
+
+conn.commit()
+
