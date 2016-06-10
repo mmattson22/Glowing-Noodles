96f9389b4311f152e89124c7f5719f24ecfee250
freepina <freepina@gmail.com>
2016-06-10 04:31:23 -0400

    3

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def found():
 @app.route('/lost/<int:post_id>', methods = ['GET','POST'])
 def post(post_id):
     if request.method=="GET":
-        post = utils.getPost(post_id,"lost")
-        comments = utils.getCommentsOnPost(post_id)
+        post = utils.getPost(int(post_id),"lost")
+        comments = utils.getCommentsOnPost(int(post_id))
 
         return render_template("post.html",post=post, comments = comments)
     else:
